afc92f808064e6c72b219b15c0a904c4b46254bc
Vitalii Ananev <vananiev@users.noreply.github.com>
2022-10-23 03:14:17 +0300

    fix iterator

--- src/main/java/org/spacious_team/table_wrapper/api/AbstractTable.java
+++ src/main/java/org/spacious_team/table_wrapper/api/AbstractTable.java
@@ -31,6 +31,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
@@ -232,6 +233,9 @@ public abstract class AbstractTable<R extends ReportPageRow> implements Table {
 
         @Override
         public TableRow next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
             int rowNum;
             @Nullable R row;
             do {
